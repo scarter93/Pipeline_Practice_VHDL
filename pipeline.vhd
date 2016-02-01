da398625cd381828b771f200f2baa81325a6251c
Stephen carter <stephen.carter@mail.mcgill.ca>
2016-02-01 11:45:23 -0500

    pipeline and testbed completed

--- pipeline.vhd
+++ pipeline.vhd
@@ -10,11 +10,31 @@ port (clk : in std_logic;
 end pipeline;
 
 architecture behavioral of pipeline is
+--Temporary values for pipelined operations
+Signal op1_t, op2_t, op3_t, op4_t, op5_t, final_output_t : integer := 0;
 
 begin
--- todo: complete this
+--Assign output to temporary values
+op1 <= op1_t;
+op2 <= op2_t;
+op3 <= op3_t;
+op4 <= op4_t;
+op5 <= op5_t;
+final_output <= final_output_t;
+
+-- perform computation (pipelined)
 process (clk)
 begin
+	if rising_edge(clk) then
+		op1_t <= a + b;
+		op3_t <= c * d;
+		op4_t <= a - e;
+
+		op2_t <= op1_t * 42;
+		op5_t <= op3_t * op4_t;
+
+		final_output_t <= op2_t - op5_t;
+	end if;
 end process;
 
 end behavioral;
--- pipeline_tb.vhd
+++ pipeline_tb.vhd
@@ -37,7 +37,100 @@ END PROCESS;
 
 stim_process: PROCESS
 BEGIN   
-	--TODO: Stimulate the inputs for the pipelined equation ((a + b) * 42) - (c * d * (a - e)) and assert the results
+	--Stimulate the inputs for the pipelined equation ((a + b) * 42) - (c * d * (a - e)) and assert the results
+	REPORT "test cases";
+	--load first case
+	s_a <= 1;
+	s_b <= 2;
+	s_c <= 3;
+	s_d <= 4;
+	s_e <= 5;
+	
+	WAIT FOR 1 * clk_period;	
+	-- as pipeline fills
+	ASSERT(s_op1 = 3) REPORT "OP1 failed" SEVERITY ERROR;
+	ASSERT(s_op3 = 12) REPORT "OP3 failed" SEVERITY ERROR;
+	ASSERT(s_op4 = -4) REPORT "OP4 failed" SEVERITY ERROR;
+	--load next case after checking output of first pipeline stage
+	s_a <= 6;
+	s_b <= 2;
+	s_c <= 3;
+	s_d <= 4;
+	s_e <= 5;
+	
+	WAIT FOR 1 * clk_period;	
+	--as pipeline fills
+	ASSERT(s_op1 = 8) REPORT "OP1 failed" SEVERITY ERROR;
+	ASSERT(s_op3 = 12) REPORT "OP3 failed" SEVERITY ERROR;
+	ASSERT(s_op4 = 1) REPORT "OP4 failed" SEVERITY ERROR;
+
+	ASSERT(s_op2 = 126) REPORT "OP2 failed" SEVERITY ERROR;
+	ASSERT(s_op5 = -48) REPORT "OP5 failed" SEVERITY ERROR;
+	--load next case, after checking previous values
+	s_a <= 1;
+	s_b <= 2;
+	s_c <= 3;
+	s_d <= 4;
+	s_e <= 5;
+	
+	WAIT FOR 1 * clk_period;	
+	--full pipeline
+	ASSERT(s_op1 = 3) REPORT "OP1 failed" SEVERITY ERROR;
+	ASSERT(s_op3 = 12) REPORT "OP3 failed" SEVERITY ERROR;
+	ASSERT(s_op4 = -4) REPORT "OP4 failed" SEVERITY ERROR;
+
+	ASSERT(s_op2 = 336) REPORT "OP2 failed" SEVERITY ERROR;
+	ASSERT(s_op5 = 12) REPORT "OP5 failed" SEVERITY ERROR;
+
+	ASSERT(s_final_output = 174) REPORT "final_output failed" SEVERITY ERROR;
+	--load next case, after checking previous pipeline values
+	s_a <= 4;
+	s_b <= 1;
+	s_c <= 6;
+	s_d <= 8;
+	s_e <= -1;
+
+	WAIT FOR 1 * clk_period;
+	--full pipeline
+	ASSERT(s_op1 = 5) REPORT "OP1 failed" SEVERITY ERROR;
+	ASSERT(s_op3 = 48) REPORT "OP3 failed" SEVERITY ERROR;
+	ASSERT(s_op4 = 5) REPORT "OP4 failed" SEVERITY ERROR;
+
+	ASSERT(s_op2 = 126) REPORT "OP2 failed" SEVERITY ERROR;
+	ASSERT(s_op5 = -48) REPORT "OP5 failed" SEVERITY ERROR;
+
+	ASSERT(s_final_output = 324) REPORT "final_output failed" SEVERITY ERROR;
+	-- load final case after checking the values from earlier
+	s_a <= -1;
+	s_b <= -2;
+	s_c <= 7;
+	s_d <= -4;
+	s_e <= 5;
+
+	WAIT FOR 1 * clk_period;
+	-- assert values to ensure effective pipelining (full pipeline)
+	ASSERT(s_op1 = -3) REPORT "OP1 failed" SEVERITY ERROR;
+	ASSERT(s_op3 = -28) REPORT "OP3 failed" SEVERITY ERROR;
+	ASSERT(s_op4 = -6) REPORT "OP4 failed" SEVERITY ERROR;
+
+	ASSERT(s_op2 = 210) REPORT "OP2 failed" SEVERITY ERROR;
+	ASSERT(s_op5 = 240) REPORT "OP5 failed" SEVERITY ERROR;
+
+	ASSERT(s_final_output = 174) REPORT "final_output failed" SEVERITY ERROR;
+
+	WAIT FOR 1 * clk_period;
+	-- assert values to ensure effective pipelining
+	-- pipeline draining
+	ASSERT(s_op2 = -126) REPORT "OP2 failed" SEVERITY ERROR;
+	ASSERT(s_op5 = 168) REPORT "OP5 failed" SEVERITY ERROR;
+
+	ASSERT(s_final_output = -30) REPORT "final_output failed" SEVERITY ERROR;
+
+	WAIT FOR 1 * clk_period;
+	-- assert values finalvalue to ensure last operation completed successfully
+	-- pipeline drained
+	ASSERT(s_final_output = -294) REPORT "final_output failed" SEVERITY ERROR;
+
 	WAIT;
 END PROCESS stim_process;
 END;
